0914202103c05b8e709f4cd3e10d24da56ab88c9
mplouffe <matheuplouffe@gmail.com>
2018-07-08 16:32:16 -0500

    Testing moving deployment to Heroku.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
 var questionController = require('./controllers/questionController');
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
   res.send("Running...");
