9475d6f38750be46f06b3f89e7ecbf604d8e1549
mplouffe <matheuplouffe@gmail.com>
2018-07-08 16:07:27 -0500

    Troubleshooting OpenShift Deployment...

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ var app = express();
 var questionController = require('./controllers/questionController');
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.get('/', function(req, res){
+  res.send("Running...");
+});
 questionController(app);
 app.listen(port, server_ip_address);
